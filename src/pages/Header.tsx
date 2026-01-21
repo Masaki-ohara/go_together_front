64b69c10009e49e04fad45c2107dacbc4ddd9771
小原雅樹 <oharamasaki@oharamasakinoMacBook-Air.local>
2026-01-21 17:01:26 +0900

    サイドバー作成

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Header() {
     return (    
-<div className="relative mb-6 flex items-center justify-between gap-6 pt-2 bg-sky-600 h-20">
+<header className="h-14 bg-sky-600 text-white flex items-center px-4">
       <div className="absolute inset-0 flex justify-center ml-4 md:static">ロゴ</div>
       <div className="hidden md:block ml-4">検索フォーム</div>
       <div className="flex justify-center ml-4">go_together</div>
       <div className="ml-auto">ボタン</div>
-      <a href="/signup" className="mr-4">ユーザー登録</a>
-    </div>
+      <a href="/signup" className="mr-4 ml-4">ユーザー登録</a>
+    </header>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Header from './Header';
+// Home.tsx
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
-export default function Home () {
-    return (
-        <div>
-            <Header />
-            <h1>Home Page</h1>
-        </div>
-  )
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col">
+      {/* Header */}
+      <Header />
+
+      {/* Body */}
+      <div className="flex flex-1">
+        <Sidebar />
+
+        <main className="flex-1 p-4 bg-white">
+          <h1 className="text-2xl font-bold">Home Page</h1>
+        </main>
+      </div>
+    </div>
+  );
 }
-
